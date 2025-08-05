4b2ab4079b524c1fc8f61c95f1957a9cd494e33d
Cleber Souza <cleber.constante@totvs.com.br>
2025-08-05 14:00:09 -0300

    fix: parse

--- src/main/java/br/com/ccs/rinha/model/input/PaymentRequest.java
+++ src/main/java/br/com/ccs/rinha/model/input/PaymentRequest.java
@@ -28,19 +28,10 @@ public final class PaymentRequest {
     }
 
     public PaymentRequest parse(String json) {
-        var chars = json.toCharArray();
-
-        int idx = json.indexOf("\"correlationId\":\"") + 17;
-        int end = json.indexOf('"', idx);
-//        req.correlationId = json.substring(idx, end);
-
-        idx = json.indexOf("\"amount\":", end) + 9;
-        while (chars[idx] == ' ' || chars[idx] == '\t') idx++;
-
-        int startAmount = idx;
-        while ((chars[idx] >= '0' && chars[idx] <= '9') || chars[idx] == '.' || chars[idx] == '-') idx++;
-
-        this.amount = new BigDecimal(json.substring(startAmount, idx));
+        int startAmount = json.lastIndexOf(':') + 1;
+        int endAmount = json.length() - 1;
+        var strAmount = json.substring(startAmount, endAmount);
+        this.amount = new BigDecimal(strAmount);
         this.requestedAt = Instant.parse(json.substring(16, json.lastIndexOf("Z") + 1)).toEpochMilli();
 
         return this;
@@ -63,16 +54,14 @@ public final class PaymentRequest {
     private static final byte[] REQUESTED_AT_SUFFIX = "\",".getBytes(StandardCharsets.UTF_8);
 
     private static final ThreadLocal<ByteBuffer> BUFFER = ThreadLocal.withInitial(() -> ByteBuffer.allocate(256));
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ISO_INSTANT.withZone(ZoneOffset.UTC);
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_INSTANT.withZone(ZoneOffset.UTC);
 
     public byte[] getPostData() {
         if (postData == null) {
             Instant now = Instant.ofEpochMilli(System.currentTimeMillis());
             var timestamp = FORMATTER.format(now).getBytes(StandardCharsets.UTF_8);
 
-            ByteBuffer buffer = BUFFER.get();
-            buffer.clear();
+            ByteBuffer buffer = BUFFER.get().clear();
 
             buffer.put(REQUESTED_AT_PREFIX);
             buffer.put(timestamp);
